abf5fb4237727bf3b978871061615f959969ff7c
Racheal <rachealnjugush@gmail.com>
2024-11-13 08:58:18 +0300

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-function Search(){
 
+function Search(searchforme){
+    //HANDLESEARCH
+    function handleSearch(e){
+        searchforme(e.target.name)
+    }
+    return(
+        <div className='search-me'>
+            <label htmlFor='search'>Search me...</label>
+            <input type='text' id='search' placeholder='search for me ...' onChange={handleSearch}/>
+        </div>
+    )
 }
 
 export default Search
